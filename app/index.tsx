f010b33aa6d92869683e6929d3814709fc7bc6ca
Brij B <brijbhatia997@gmail.com>
2025-01-09 17:37:50 -0800

    Updated Index and SignUp

--- app/index.tsx
+++ app/index.tsx
@@ -8,6 +8,14 @@ import {
 import React from "react";
 import { Link } from "expo-router";
 import { LinearGradient } from "expo-linear-gradient";
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import Google from "@/assets/images/google-logo.svg";
+import Animated, {
+  FadeIn,
+  FadeInDown,
+  FadeInRight,
+} from "react-native-reanimated";
 
 type Props = {};
 
@@ -28,17 +36,52 @@ const WelcomeScreen = (props: Props) => {
           style={styles.background}
         >
           <View style={styles.wrapper}>
-            <Text>Welcome Screen</Text>
-            <Link href={"/signin"} asChild>
-              <TouchableOpacity>
-                <Text>Go to SignIn Screen</Text>
-              </TouchableOpacity>
-            </Link>
-            <Link href={"/signup"} asChild>
-              <TouchableOpacity>
-                <Text>Go to SignUp Screen</Text>
-              </TouchableOpacity>
-            </Link>
+            <Animated.Text
+              style={styles.title}
+              entering={FadeInRight.delay(300).duration(300).springify()}
+            >
+              ShopX
+            </Animated.Text>
+            <Animated.Text
+              style={styles.description}
+              entering={FadeInRight.delay(500).duration(300).springify()}
+            >
+              Discover Your Style
+            </Animated.Text>
+            <View style={styles.socialLoginWrapper}>
+              <Animated.View entering={FadeInDown.delay(300).duration(500)}>
+                <Link href={"/signup"} asChild>
+                  <TouchableOpacity style={styles.button}>
+                    <Ionicons
+                      name="mail-outline"
+                      size={20}
+                      color={Colors.black}
+                    />
+                    <Text style={styles.btnText}>Continue with Email</Text>
+                  </TouchableOpacity>
+                </Link>
+              </Animated.View>
+              <Animated.View entering={FadeInDown.delay(700).duration(500)}>
+                <TouchableOpacity style={styles.button}>
+                  <Google width={20} height={20} />
+                  <Text style={styles.btnText}>Continue with Google</Text>
+                </TouchableOpacity>
+              </Animated.View>
+              <Animated.View entering={FadeInDown.delay(1100).duration(500)}>
+                <TouchableOpacity style={styles.button}>
+                  <Ionicons name="logo-apple" size={20} color={Colors.black} />
+                  <Text style={styles.btnText}>Continue with Apple</Text>
+                </TouchableOpacity>
+              </Animated.View>
+            </View>
+            <Text style={styles.loginTxt}>
+              Already have an account?{" "}
+              <Link href={"/signin"} asChild>
+                <TouchableOpacity>
+                  <Text style={styles.loginTxtSpan}>SignIn</Text>
+                </TouchableOpacity>
+              </Link>
+            </Text>
           </View>
         </LinearGradient>
       </View>
@@ -67,5 +110,47 @@ const styles = StyleSheet.create({
     paddingBottom: 50,
     paddingHorizontal: 20,
     alignItems: "center",
+  },
+  title: {
+    fontSize: 22,
+    color: Colors.primary,
+    fontWeight: "700",
+    letterSpacing: 2.4,
+    marginBottom: 5,
+  },
+  description: {
+    fontSize: 14,
+    color: Colors.gray,
+    lineHeight: 30,
+    marginBottom: 20,
+  },
+  socialLoginWrapper: {
+    alignSelf: "stretch",
+  },
+  button: {
+    flexDirection: "row",
+    padding: 10,
+    borderColor: Colors.gray,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
+    marginBottom: 15,
+  },
+  btnText: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: Colors.black,
+  },
+  loginTxt: {
+    marginTop: 30,
+    fontSize: 14,
+    lineHeight: 24,
+    color: Colors.black,
+  },
+  loginTxtSpan: {
+    color: Colors.primary,
+    fontWeight: "600",
   },
 });
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,22 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { router, Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "@/constants/Colors";
 
-type Props = {}
+type Props = {};
 
 const SignUpScreen = (props: Props) => {
   return (
-    <View style={styles.container}>
-      <Text>SignUp Screen</Text>
-    </View>
-  )
-}
+    <>
+      <Stack.Screen
+        options={{
+          headerTitle: "Sign Up",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close" size={24} color={Colors.black} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <View style={styles.container}>
+        <Text>Create an account</Text>
+      </View>
+    </>
+  );
+};
 
-export default SignUpScreen
+export default SignUpScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
