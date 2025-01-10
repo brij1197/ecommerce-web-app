78f3aa438ea3a2424d7316a04e6dfd471cd33e53
Brij Bhatia <brijbhatia997@gmail.com>
2025-01-09 19:26:42 -0800

    Added Animated Style for Tabbar

--- components/TabBarButton.tsx
+++ components/TabBarButton.tsx
@@ -19,6 +19,12 @@ const TabBarButton = (props: Props) => {
       onLongPress={onLongPress}
       style={styles.tabbarBtn}
     >
+      {routeName === "cart" && (
+        <View style={styles.badgeWrapper}>
+          <Text style={styles.badgeText}>3</Text>
+        </View>
+      )}
+
       {icon[routeName]({
         color: isFocused ? Colors.primary : Colors.black,
       })}
@@ -33,6 +39,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     gap: 5,
+  },
+  badgeWrapper: {
+    position: "absolute",
+    color: Colors.highlight,
+    top: -5,
+    right: 20,
+    paddingVertical: 2,
+    paddingHorizontal: 6,
+    borderRadius: 10,
+    zIndex: 10,
+  },
+  badgeText: {
+    color:Colors.black,
+    fontSize: 12,
   },
 });
 
