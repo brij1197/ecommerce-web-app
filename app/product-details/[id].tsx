0e405181757d69032878f8e5f0a127e9a384f32d
Brij Bhatia <brijbhatia997@gmail.com>
2025-01-10 01:32:41 -0800

    Updated id file

--- app/product-details/[id].tsx
+++ app/product-details/[id].tsx
@@ -1,17 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { useLocalSearchParams } from "expo-router";
+import db from "../../data/db.json";
 
-type Props = {}
+type Props = {};
 
 const ProductDetails = (props: Props) => {
-  const {id}=useLocalSearchParams();
+  const { id } = useLocalSearchParams();
+
+  useEffect(() => {
+    getProductDetails(id);
+  }, []);
+
+  const getProductDetails = (id: any) => {
+    try {
+      const product = db.products.find((product:any) => product.id === id);
+      console.log(product);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View>
       <Text>ProductDetails - {id}</Text>
     </View>
-  )
-}
+  );
+};
 
-
-export default ProductDetails
+export default ProductDetails;
