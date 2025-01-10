401e03be21a1f9aed85b9430e0faedd8617fe6ed
Brij Bhatia <brijbhatia997@gmail.com>
2025-01-10 03:49:30 -0800

    Updated Buttons in Product Pages

--- app/product-details/[id].tsx
+++ app/product-details/[id].tsx
@@ -1,10 +1,17 @@
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
-import { useLocalSearchParams } from "expo-router";
+import { router, Stack, useLocalSearchParams } from "expo-router";
 import { ProductType } from "@/types/type";
 import ImageSlider from "@/components/ImageSlider";
 import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "@/constants/Colors";
+import { useHeaderHeight } from "@react-navigation/elements";
 
 type Props = {};
 
@@ -33,113 +40,167 @@ const ProductDetails = (props: Props) => {
     }
   };
 
+  const headerHeight = useHeaderHeight();
+
   return (
-    <ScrollView>
-      {product && (
-        <>
-          <ImageSlider imageList={product.images} />
-          <View style={styles.container}>
-            <View style={styles.ratingWrapper}>
+    <>
+      <Stack.Screen
+        options={{
+          title: "Product Details",
+          headerTransparent: true,
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="arrow-back" size={24} color={Colors.black} />\
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity>
+              <Ionicons name="cart-outline" size={24} colors={Colors.black} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <ScrollView style={{ marginTop: headerHeight, marginBottom: 90 }}>
+        {product && (
+          <>
+            <ImageSlider imageList={product.images} />
+            <View style={styles.container}>
               <View style={styles.ratingWrapper}>
-                <Ionicons name="star" size={18} color={"#D4AF37"} />
-                <Text style={styles.rating}>
-                  4.7 <Text>(136)</Text>
-                </Text>
+                <View style={styles.ratingWrapper}>
+                  <Ionicons name="star" size={18} color={"#D4AF37"} />
+                  <Text style={styles.rating}>
+                    4.7 <Text>(136)</Text>
+                  </Text>
+                </View>
+                <TouchableOpacity>
+                  <Ionicons
+                    name="heart-outline"
+                    size={20}
+                    color={Colors.black}
+                  />
+                </TouchableOpacity>
               </View>
-              <TouchableOpacity>
-                <Ionicons name="heart-outline" size={20} color={Colors.black} />
-              </TouchableOpacity>
-            </View>
-            <Text style={styles.title}>{product.title}</Text>
-            <View style={styles.priceWrapper}>
-              <Text style={styles.price}>${product.price}</Text>
-              <View style={styles.priceDiscount}>
-                <Text style={styles.priceDiscountText}>6% Off</Text>
+              <Text style={styles.title}>{product.title}</Text>
+              <View style={styles.priceWrapper}>
+                <Text style={styles.price}>${product.price}</Text>
+                <View style={styles.priceDiscount}>
+                  <Text style={styles.priceDiscountText}>6% Off</Text>
+                </View>
+                <Text style={styles.oldPrice}>${product.price + 2}</Text>
               </View>
-              <Text style={styles.oldPrice}>${product.price + 2}</Text>
-            </View>
-            <Text style={styles.description}>{product.description}</Text>
-            <View style={styles.productVariationWrapper}>
-              <View style={styles.productVariationType}>
-                <Text style={styles.productVariationTitle}>Color</Text>
-                <View style={styles.productVariationValueWrapper}>
-                  <View
-                    style={{
-                      borderColor: Colors.primary,
-                      borderWidth: 1,
-                      borderRadius: 100,
-                      padding: 2,
-                    }}
-                  >
+              <Text style={styles.description}>{product.description}</Text>
+              <View style={styles.productVariationWrapper}>
+                <View style={styles.productVariationType}>
+                  <Text style={styles.productVariationTitle}>Color</Text>
+                  <View style={styles.productVariationValueWrapper}>
+                    <View
+                      style={{
+                        borderColor: Colors.primary,
+                        borderWidth: 1,
+                        borderRadius: 100,
+                        padding: 2,
+                      }}
+                    >
+                      <View
+                        style={[
+                          styles.productVariationColorValue,
+                          { backgroundColor: "#D4AF37" },
+                        ]}
+                      />
+                    </View>
                     <View
                       style={[
                         styles.productVariationColorValue,
-                        { backgroundColor: "#D4AF37" },
+                        { backgroundColor: "#333" },
+                      ]}
+                    />
+                    <View
+                      style={[
+                        styles.productVariationColorValue,
+                        { backgroundColor: "#8bc34a" },
+                      ]}
+                    />
+                    <View
+                      style={[
+                        styles.productVariationColorValue,
+                        { backgroundColor: "#2196f3" },
+                      ]}
+                    />
+                    <View
+                      style={[
+                        styles.productVariationColorValue,
+                        { backgroundColor: "#f44336" },
+                      ]}
+                    />
+                    <View
+                      style={[
+                        styles.productVariationColorValue,
+                        { backgroundColor: "#9c27b0" },
                       ]}
                     />
                   </View>
-                  <View
-                    style={[
-                      styles.productVariationColorValue,
-                      { backgroundColor: "#333" },
-                    ]}
-                  />
-                  <View
-                    style={[
-                      styles.productVariationColorValue,
-                      { backgroundColor: "#8bc34a" },
-                    ]}
-                  />
-                  <View
-                    style={[
-                      styles.productVariationColorValue,
-                      { backgroundColor: "#2196f3" },
-                    ]}
-                  />
-                  <View
-                    style={[
-                      styles.productVariationColorValue,
-                      { backgroundColor: "#f44336" },
-                    ]}
-                  />
-                  <View
-                    style={[
-                      styles.productVariationColorValue,
-                      { backgroundColor: "#9c27b0" },
-                    ]}
-                  />
                 </View>
-              </View>
-              <View style={styles.productVariationType}>
-                <Text style={styles.productVariationTitle}>Size</Text>
-                <View style={styles.productVariationValueWrapper}>
-                  <View style={[styles.productVariationSizeValue,{borderColor:Colors.primary}]}>
-                    <Text style={[styles.productVariationSizeValueText,{color:Colors.primary,fontWeight:'bold'}]}>
-                      S
-                    </Text>
-                  </View>
-                  <View style={styles.productVariationSizeValue}>
-                    <Text style={styles.productVariationSizeValueText}>
-                      M
-                    </Text>
-                  </View>
-                  <View style={styles.productVariationSizeValue}>
-                    <Text style={styles.productVariationSizeValueText}>
-                      L
-                    </Text>
-                  </View>
-                  <View style={styles.productVariationSizeValue}>
-                    <Text style={styles.productVariationSizeValueText}>
-                      XL
-                    </Text>
+                <View style={styles.productVariationType}>
+                  <Text style={styles.productVariationTitle}>Size</Text>
+                  <View style={styles.productVariationValueWrapper}>
+                    <View
+                      style={[
+                        styles.productVariationSizeValue,
+                        { borderColor: Colors.primary },
+                      ]}
+                    >
+                      <Text
+                        style={[
+                          styles.productVariationSizeValueText,
+                          { color: Colors.primary, fontWeight: "bold" },
+                        ]}
+                      >
+                        S
+                      </Text>
+                    </View>
+                    <View style={styles.productVariationSizeValue}>
+                      <Text style={styles.productVariationSizeValueText}>
+                        M
+                      </Text>
+                    </View>
+                    <View style={styles.productVariationSizeValue}>
+                      <Text style={styles.productVariationSizeValueText}>
+                        L
+                      </Text>
+                    </View>
+                    <View style={styles.productVariationSizeValue}>
+                      <Text style={styles.productVariationSizeValueText}>
+                        XL
+                      </Text>
+                    </View>
                   </View>
                 </View>
               </View>
             </View>
-          </View>
-        </>
-      )}
-    </ScrollView>
+          </>
+        )}
+      </ScrollView>
+      <View style={styles.buttonWrapper}>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            {
+              backgroundColor: Colors.white,
+              borderColor: Colors.primary,
+              borderWidth: 1,
+            },
+          ]}
+        >
+          <Ionicons name="cart-outline" size={20} color={Colors.primary} />
+          <Text style={[styles.buttonText, { color: Colors.primary }]}>
+            Add to Cart
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Buy Now</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 
@@ -242,6 +303,36 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: "500",
     color: Colors.black,
+  },
+  buttonWrapper: {
+    position: "absolute",
+    bottom: 0,
+    flexDirection: "row",
+    padding: 20,
+    gap: 10,
+    height: 90,
+    width: "100%",
+    backgroundColor: Colors.white,
+  },
+  button: {
+    flex: 1,
+    backgroundColor: Colors.primary,
+    height: 40,
+    gap: 5,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    elevation: 5,
+    shadowColor: Colors.black,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: Colors.white,
   },
 });
 
