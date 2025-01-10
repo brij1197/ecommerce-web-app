a4abe70bf60c664eeb1ac42f0f6ad7d28bf48b1d
Brij Bhatia <brijbhatia997@gmail.com>
2025-01-10 02:57:56 -0800

    Updated Products Page

--- app/product-details/[id].tsx
+++ app/product-details/[id].tsx
@@ -1,8 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { ProductType } from "@/types/type";
 import ImageSlider from "@/components/ImageSlider";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "@/constants/Colors";
 
 type Props = {};
 
@@ -36,11 +38,63 @@ const ProductDetails = (props: Props) => {
       {product && (
         <>
           <ImageSlider imageList={product.images} />
-          <Text>{product.title}</Text>
+          <View style={styles.container}>
+            <View style={styles.ratingWrapper}>
+              <View style={styles.ratingWrapper}>
+                <Ionicons name="star" size={18} color={"#D4AF37"} />
+                <Text style={styles.rating}>
+                  4.7 <Text>(136)</Text>
+                </Text>
+              </View>
+              <TouchableOpacity>
+                <Ionicons name="heart-outline" size={20} color={Colors.black} />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.title}>{product.title}</Text>
+            <View style={styles.priceWrapper}>
+              <Text style={styles.price}>${product.price}</Text>
+            </View>
+          </View>
         </>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+  },
+  ratingWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 5,
+  },
+  rating: {
+    marginLeft: 5,
+    fontSize: 14,
+    fontWeight: "400",
+    color: Colors.gray,
+  },
+  title:{
+    fontSize: 20,
+    fontWeight: "400",
+    color: Colors.black,
+    letterSpacing: 0.6,
+    lineHeight: 32,
+  },
+  priceWrapper:{
+    flexDirection: "row",
+    alignItems: "center",
+    gap:5,
+    marginTop: 20,
+  },
+  price:{
+    fontSize: 18,
+    fontWeight: "600",
+    color: Colors.black,
+  }
+});
 
 export default ProductDetails;
