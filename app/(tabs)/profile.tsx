c362a824ffc32dcc40af11ee0778fab10677728d
Brij Bhatia <brijbhatia997@gmail.com>
2025-01-10 06:03:39 -0800

    Fixed Buttons Profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,9 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
 import { useHeaderHeight } from "@react-navigation/elements";
 import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 type Props = {};
 
@@ -21,6 +22,40 @@ const ProfileScreen = (props: Props) => {
           />
           <Text style={styles.userName}>John Doe</Text>
         </View>
+        <View style={styles.buttonWrapper}>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name='person-outline' size={20} color={Colors.black}/>
+            <Text style={styles.buttonText}>Your Orders</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name='heart-outline' size={20} color={Colors.black}/>
+            <Text style={styles.buttonText}>Your Wishlist</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name='card-outline' size={20} color={Colors.black}/>
+            <Text style={styles.buttonText}>Payment History</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name='gift-outline' size={20} color={Colors.black}/>
+            <Text style={styles.buttonText}>Reward Point</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name='help-circle-outline' size={20} color={Colors.black}/>
+            <Text style={styles.buttonText}>Customer Support</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name='pencil-outline' size={20} color={Colors.black}/>
+            <Text style={styles.buttonText}>Edit Profile</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name='settings-outline' size={20} color={Colors.black}/>
+            <Text style={styles.buttonText}>Settings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name='log-out-outline' size={20} color={Colors.black}/>
+            <Text style={styles.buttonText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </>
   );
@@ -37,6 +72,24 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "500",
     marginTop: 10,
+    color: Colors.black,
+  },
+  buttonWrapper: {
+    marginTop: 20,
+    gap:10
+  },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.lightGray,
+    borderRadius: 5,
+    padding: 10,
+    borderWidth:1,
+    gap:10
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "500",
     color: Colors.black,
   },
 });
