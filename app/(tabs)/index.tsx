2099d485655b700c714e8c5608fb364bccc89b3a
Brij Bhatia <brijbhatia997@gmail.com>
2025-01-09 19:50:22 -0800

    Updated layout and index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ProductType } from "@/types/type";
 
-type Props = {}
+type Props = {};
 
 const HomeScreen = (props: Props) => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await require("../../data/db.json");
+        setProducts(response.products);
+        setLoading(false);
+      } catch (error) {
+        console.error("Failed to load products:", error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ index, item }) => <Text>{item.title}</Text>}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
