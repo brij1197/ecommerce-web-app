0cc5d762fb92d1e5db9f3b2151a8dcf4e372fb09
Brij Bhatia <brijbhatia997@gmail.com>
2025-01-10 02:03:59 -0800

    Added Pagination

---
+++ components/Pagination.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Pagination() {
+  return (
+    <View>
+      <Text>Pagination</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
